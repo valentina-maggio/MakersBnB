e0d9892cb2102d658af3dadc3373e02debe62ab8
Valentina <maggio.vale@gmail.com>
2022-03-22 12:39:33 +0000

    Create sign up
    Co-authored-by: Felix Taljaard <felixtaljaard@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -16,12 +16,21 @@ class MakersBnB < Sinatra::Base
 
   get '/' do
     # Get all the spaces listed by the user with id 1
-    User.find_by(id: 1).spaces
+    #User.find_by(id: 1).spaces
  
-    
-
     # "SELECT first_name FROM spaces JOIN users ON (spaces.user_id = users.id);"
     erb(:index)
+  end
+
+  get '/registrations/new' do
+    erb(:'registrations/new')
+  end
+  
+  post '/registrations' do
+    user = User.new(email: params[:email], first_name: params[:first_name], last_name: params[:last_name], password: "")
+    user.password = params[:password]
+    user.save!
+    redirect('/')
   end
 
   run! if app_file == $PROGRAM_NAME
