31db11725a1e2501fbd9610debaa37e72f3c32eb
Joe Thornton <thornton.jp@gmail.com>
2022-03-22 15:56:43 +0000

    added page for creating a listing

---
+++ spec/feature/add_listing_feature_spec.rb
@@ -0,0 +1,23 @@
+feature 'add listing' do
+  scenario 'user can add a listing to the list of spaces' do
+
+    visit '/spaces/list'
+
+    fill_in 'name', with: 'the pyramids'
+    fill_in 'description', with: 'old-fashioned interior'
+    fill_in 'price', with: 500000
+    within '#dates_form' do
+      fill_in name: 'available_from', with: '2022-04-01'
+    end
+    within '#dates_form' do
+      fill_in name: 'available_to', with: '2022-05-01'
+    end
+
+    click_button 'Submit'
+
+    expect(current_path).to eq '/spaces'
+
+    expect(page).to have_content 'the pyramids'
+    expect(page).to have_content 'old-fashioned interior'
+  end
+end
